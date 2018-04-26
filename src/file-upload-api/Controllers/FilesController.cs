d728a91a871acaf387461e5870041f403314c8db
Birdperson90 <al.khatib.sami1990@gmail.com>
2018-04-26 11:20:07 +0200

    initial commit

---
+++ src/file-upload-api/Controllers/FilesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace file_upload_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        [HttpPost("upload")]
+        public IActionResult Upload([FromForm] IEnumerable<IFormFile> files)
+        {
+            return new ObjectResult(new
+            {
+                Names = string.Join(", ", files.Select(file => file.FileName).ToArray()),
+                TotalSize = files.Select(file => file.Length).Sum()
+            });
+        }
+    }
+}
